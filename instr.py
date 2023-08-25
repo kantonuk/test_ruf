839273c18f85b4d1b11f82ec185cd1b93e3d1ca6
kantonuk <kantonuk269@gmail.com>
2023-08-25 19:32:37 +0300

    Add new classes for main win and create files

---
+++ instr.py
@@ -0,0 +1,3 @@
+title_txt="Здоров'я"
+win_x,win_y=200,100
+win_width,win_height=1000,600
--- main_app.py
+++ main_app.py
@@ -1,3 +1,21 @@
-def hello():
-  print("hello")
-hello()
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (QApplication,QWidget,
+                             QHBOxLayout ,QVBoxLayout,QGroupDox,QRadioButton,
+                             QLineEdit )
+
+from instr import*
+
+class Main_win(QWidget):
+    def set_appear(self):
+        self.setWindowTitle(title_txt)
+        self.resize(win_width,win_height)
+        self.move(win_x,win_y)
+    
+
+
+    def initUI(self):
+        pass
+    
+
+    def connects(self)
+        pass
